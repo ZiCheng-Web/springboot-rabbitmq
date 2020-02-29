be51edf0f4b16b32ac4c33978f9472464818c34e
ZiCheng-Web <523186180@qq.com>
2020-03-01 00:36:43 +0800

    rabbitMQ

---
+++ src/main/java/com/zicheng/domain/User.java
@@ -0,0 +1,14 @@
+package com.zicheng.domain;
+
+import lombok.Data;
+
+/**
+ * 子诚
+ * Description：
+ * 时间：2020/2/29 22:14
+ */
+@Data
+public class User {
+    private Integer id;
+    private String username;
+}
